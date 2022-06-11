b39404c865ed53769ce35c3afb6f1ff526ce7f41
eddie ciccone <ecicc2@uis.edu>
2022-06-11 13:56:26 -0500

    page component now shows if a page is loading or an error occured

--- src/components/page/Page.js
+++ src/components/page/Page.js
@@ -1,9 +1,19 @@
 import './Page.css';
 
-export const Page = ({ title = "", children }) => {
+export const Page = ({ title = "", loading = false, error = null, children }) => {
+
+  if (error) {
+    return <div className="page">{ error }</div>
+  }
+
+  if (loading) {
+    return <div className="page">Loading...</div>
+  }
+
   return (
     <div className="page">
       <h1>{ title }</h1>
+      
       { children }
     </div>
   );
