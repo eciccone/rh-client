5bdd88cd814e60ca15e0ed99c4a7aa89193828b0
eddie ciccone <ecicc2@uis.edu>
2022-06-11 13:59:50 -0500

    page component error is now displayed with children

--- src/components/page/Page.js
+++ src/components/page/Page.js
@@ -2,16 +2,15 @@ import './Page.css';
 
 export const Page = ({ title = "", loading = false, error = null, children }) => {
 
-  if (error) {
-    return <div className="page">{ error }</div>
-  }
-
   if (loading) {
     return <div className="page">Loading...</div>
   }
 
   return (
     <div className="page">
+
+      { error && <p>{ error }</p> }
+
       <h1>{ title }</h1>
       
       { children }
