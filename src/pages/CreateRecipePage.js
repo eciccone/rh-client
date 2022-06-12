38b104a963ae681e089a33e39b34c49e2abbb703
eddie ciccone <ecicc2@uis.edu>
2022-06-12 11:04:59 -0500

    callback for creating recipe in RecipeForm defined

--- src/pages/CreateRecipePage.js
+++ src/pages/CreateRecipePage.js
@@ -1,11 +1,37 @@
-import { withAuthenticationRequired } from "@auth0/auth0-react";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+import { useCallback, useState } from "react";
+import { useNavigate } from "react-router";
+import { PostRecipe, PutRecipeImage } from "../api/Recipe";
 import { Page } from "../components/page/Page";
 import RecipeForm from "../components/recipe/RecipeForm";
 
 function CreateRecipePage() {
+
+  const { getAccessTokenSilently } = useAuth0();
+  const navigate = useNavigate();
+  const [submitError, setSubmitError] = useState(null);
+
+  const createRecipe = useCallback(async (id, data, imageFile) => {
+    const token = await getAccessTokenSilently();
+    const res = await PostRecipe(token, data);
+    const json = await res.json();
+
+    if (!res.ok) {
+      setSubmitError(json.msg);
+    } else {
+      if (imageFile !== null) {
+        const imageFormData = new FormData();
+        imageFormData.append("image", imageFile);
+        await PutRecipeImage(token, json.recipe.id, imageFormData);
+      }
+      navigate(`/recipes/${json.recipe.id}`);
+    }
+  }, [getAccessTokenSilently, navigate])
+
   return (
     <Page title="Create a recipe">
-      <RecipeForm />
+      { submitError }
+      <RecipeForm submitCallback={createRecipe} />
     </Page>
   )
 }
